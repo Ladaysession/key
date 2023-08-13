a00ef0f8b7b74d79a5b912da4992337931008c28
Ladaysession <116974378+Ladaysession@users.noreply.github.com>
2023-08-14 02:47:07 +0300

    Реализую вывод блока
    Блок реализуцется через нажание на А. При первом нажатии он спавниться, при повторном убираться и так по кругу.
    
    Дальше буду использовать это как базу для вывода данный видео

--- additional_block.js
+++ additional_block.js
@@ -1,11 +1,11 @@
-window.onload = function spawn_additional_block() {
-	document.querySelector("#buttons").insertAdjacentHTML("afterbegin", '<div class="additional-div"> <button>HELLO WORLD!</button> <div class="additional-div-videos hidden" style="width:100px;height:100px;background:red;"> </div> </div>')
+// window.onload = function spawn_additional_block() {
+// 	document.querySelector("#buttons").insertAdjacentHTML("afterbegin", '<div class="additional-div"> <button>HELLO WORLD!</button> <div class="additional-div-videos hidden" style="width:100px;height:100px;background:red;"> </div> </div>')
 
-	additional_div = document.querySelector('.additional-div');
+// 	additional_div = document.querySelector('.additional-div');
 
-additional_div.addEventListener('click', function(event) {
-    document.querySelector('.additional-div-videos').classList.remove("hidden");
-});
+// additional_div.addEventListener('click', function(event) {
+//     document.querySelector('.additional-div-videos').classList.remove("hidden");
+// });
 
 // window.onclick = function(event) {
 //     if (!event.target.matches('.additional-div')) {
@@ -18,8 +18,20 @@ additional_div.addEventListener('click', function(event) {
 
 
 
-}
+// }
 
 // document.querySelector('.testtest').addEventListener('click', function(event) {
 // 	alert(123)
 // });
+
+function spawn_additional_block(){
+
+	if (document.querySelector('.additional-div') == undefined) {
+
+	document.querySelector("#buttons").insertAdjacentHTML("afterbegin", '<div class="additional-div"> <button>HELLO WORLD!</button> <div class="additional-div-videos hidden" style="width:100px;height:100px;background:red;"> </div> </div>')
+
+	} else {
+		document.querySelector('.additional-div').remove()
+	}
+
+}
