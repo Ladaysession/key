2416059a3305137c47f86f7c3dfb36fb60a3c6c2
Ladaysession <116974378+Ladaysession@users.noreply.github.com>
2023-08-13 18:39:43 +0300

    Дополнительные файлы, разбивка по структуре

--- additional_button.js
+++ additional_button.js
@@ -1,35 +1,3 @@
-//ЮТУБ
-console.log('start')
-
-document.addEventListener('keydown', function(event) {
-	//удаляем лого на видео
-  if (event.key == 'Z' ) {
-
-  	console.log('0') //этап 0 завершен, далее проверяем что сайт - ютуб
-
-    if (document.querySelector('.annotation-type-custom')) { //проверяем, это ли видео на ютубе?
-
-    	console.log('1') //этап 1 завершен, блок на ютубе удален
-
-    	document.querySelector('.annotation-type-custom').remove() //убираем блок
-    }
-  }
-
-
-  //удаляем верхний блок на главной странице
-  if (event.key == 'X' ) {
-
-    localStorage.setItem("a1", 'ZERO');
-    percent_of_views()
-  	
-  }
-
-
-
-
-  
-});
-
 array_localstorage = [0]
 
 function percent_of_views() {
---
+++ main_keys.js
@@ -0,0 +1,29 @@
+//ЮТУБ
+console.log('start')
+
+document.addEventListener('keydown', function(event) {
+	//удаляем лого на видео
+  if (event.key == 'Z' ) {
+
+  	console.log('0') //этап 0 завершен, далее проверяем что сайт - ютуб
+
+    if (document.querySelector('.annotation-type-custom')) { //проверяем, это ли видео на ютубе?
+
+    	console.log('1') //этап 1 завершен, блок на ютубе удален
+
+    	document.querySelector('.annotation-type-custom').remove() //убираем блок
+    }
+  }
+
+
+  //скрипт, анализирующий данные на странице с видео
+  if (event.key == 'X' ) {
+
+    localStorage.setItem("a1", 'ZERO');
+    percent_of_views()
+  	
+  }
+
+
+
+});
